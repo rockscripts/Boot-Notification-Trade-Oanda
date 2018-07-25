5159be81f6d70d2d1c71f8496ec0a0c799db43bd
rockscripts <rockscripts@gmail.com>
2018-07-25 16:58:21 -0200

    MACD - Macro Micro Fields added

--- database.sql
+++ database.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 13, 2018 at 08:14 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Generation Time: Jul 25, 2018 at 08:54 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -45,36 +43,13 @@ CREATE TABLE `globalConfiguration` (
   `bull` varchar(200) COLLATE utf8_bin DEFAULT NULL,
   `bear` varchar(200) COLLATE utf8_bin DEFAULT NULL,
   `macd` text COLLATE utf8_bin,
+  `macdMacro` text COLLATE utf8_bin,
+  `macdMicro` text COLLATE utf8_bin,
+  `macdStatus` varchar(200) COLLATE utf8_bin NOT NULL DEFAULT 'unotified',
   `strategy` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT 'macd',
   `candlesCount` int(11) NOT NULL DEFAULT '100',
   `candlesGranularity` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT 'H1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `globalConfiguration`
---
-ALTER TABLE `globalConfiguration`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `globalConfiguration`
---
-ALTER TABLE `globalConfiguration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 --
 -- Table structure for table `transactionPL`
@@ -89,8 +64,32 @@ CREATE TABLE `transactionPL` (
   `accountID` varchar(250) NOT NULL,
   `time` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `globalConfiguration`
+--
+ALTER TABLE `globalConfiguration`
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `transactionPL`
 --
 ALTER TABLE `transactionPL`
   ADD PRIMARY KEY (`orderID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `globalConfiguration`
+--
+ALTER TABLE `globalConfiguration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
